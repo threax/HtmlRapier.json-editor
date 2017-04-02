6376c90c0665607bdd080f8a8f758b06176a3038
Andrew Piper <andrew@threax.com>
2017-04-02 11:24:17 -0400

    Updated to npm based build.

--- build.js
+++ build.js
@@ -1,72 +1,66 @@
-﻿//json-editor
+﻿var concat = require('threax-npm-tk/concat');
 
-var compileJavascript = require('threax-gulp-tk/javascript.js');
+module.exports = function (outDir) {
+    var sourceDir = __dirname + "/../json-editor";
 
-module.exports = function (sourceDir, outDir) {
-    compileJavascript({
-        libs: [
-            // License & version info, start the containing closure
-            //sourceDir + '/src/intro.js',
-            __dirname + "/open.js",
+    return concat([
+        // License & version info, start the containing closure
+        //sourceDir + '/src/intro.js',
+        __dirname + "/open.js",
 
-            // Simple inheritance
-            sourceDir + '/src/class.js',
-            // IE9 polyfills
-            sourceDir + '/src/ie9.js',
-            // Utils like extend, each, and trigger
-            sourceDir + '/src/utilities.js',
+        // Simple inheritance
+        sourceDir + '/src/class.js',
+        // IE9 polyfills
+        sourceDir + '/src/ie9.js',
+        // Utils like extend, each, and trigger
+        sourceDir + '/src/utilities.js',
 
-            // The main JSONEditor class
-            sourceDir + '/src/core.js',
+        // The main JSONEditor class
+        sourceDir + '/src/core.js',
 
-            // JSON Schema validator
-            sourceDir + '/src/validator.js',
+        // JSON Schema validator
+        sourceDir + '/src/validator.js',
 
-            // All the editors
-            sourceDir + '/src/editor.js',
-            sourceDir + '/src/editors/null.js',
-            sourceDir + '/src/editors/string.js',
-            sourceDir + '/src/editors/number.js',
-            sourceDir + '/src/editors/integer.js',
-            sourceDir + '/src/editors/object.js',
-            sourceDir + '/src/editors/array.js',
-            sourceDir + '/src/editors/table.js',
-            sourceDir + '/src/editors/multiple.js',
-            sourceDir + '/src/editors/enum.js',
-            sourceDir + '/src/editors/select.js',
-            sourceDir + '/src/editors/selectize.js',
-            sourceDir + '/src/editors/multiselect.js',
-            sourceDir + '/src/editors/base64.js',
-            sourceDir + '/src/editors/upload.js',
-            sourceDir + '/src/editors/checkbox.js',
-            sourceDir + '/src/editors/array/selectize.js',
+        // All the editors
+        sourceDir + '/src/editor.js',
+        sourceDir + '/src/editors/null.js',
+        sourceDir + '/src/editors/string.js',
+        sourceDir + '/src/editors/number.js',
+        sourceDir + '/src/editors/integer.js',
+        sourceDir + '/src/editors/object.js',
+        sourceDir + '/src/editors/array.js',
+        sourceDir + '/src/editors/table.js',
+        sourceDir + '/src/editors/multiple.js',
+        sourceDir + '/src/editors/enum.js',
+        sourceDir + '/src/editors/select.js',
+        sourceDir + '/src/editors/selectize.js',
+        sourceDir + '/src/editors/multiselect.js',
+        sourceDir + '/src/editors/base64.js',
+        sourceDir + '/src/editors/upload.js',
+        sourceDir + '/src/editors/checkbox.js',
+        sourceDir + '/src/editors/array/selectize.js',
 
-            // All the themes and iconlibs
-            sourceDir + '/src/theme.js',
-            //sourceDir + '/src/themes/*.js',
-            sourceDir + '/src/iconlib.js',
-            //sourceDir + '/src/iconlibs/*.js',
-            sourceDir + '/src/iconlibs/bootstrap3.js',
+        // All the themes and iconlibs
+        sourceDir + '/src/theme.js',
+        //sourceDir + '/src/themes/*.js',
+        sourceDir + '/src/iconlib.js',
+        //sourceDir + '/src/iconlibs/*.js',
+        sourceDir + '/src/iconlibs/bootstrap3.js',
 
-            // The JS templating engines
-            sourceDir + '/src/templates/*.js',
+        // The JS templating engines
+        sourceDir + '/src/templates/default.js',
 
-            // Set the defaults
-            sourceDir + '/src/defaults.js',
+        // Set the defaults
+        sourceDir + '/src/defaults.js',
 
-            // Wrapper for $.fn style initialization
-            //sourceDir + '/src/jquery.js',
+        // Wrapper for $.fn style initialization
+        //sourceDir + '/src/jquery.js',
 
-            //Custom stuff
-            __dirname + "/themes/**/*.js",
+        //Custom stuff
+        __dirname + "/themes/bootstrap3custom.js",
 
-            // End the closure
-            __dirname + "/close.js",
-        ],
-        output: "json-editor",
-        dest: outDir,
-        sourceRoot: sourceDir + '/src/',
-        concat: true,
-        minify:true
-    });
+        // End the closure
+        __dirname + "/close.js",
+    ],
+    outDir + "/json-editor.js");
 };
